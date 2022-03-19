4fd962dc0ad4f75ba9782c4d84ee8a8aac73d983
Joey van der Valk <joeyvandervalk@gmail.com>
2022-03-19 14:53:48 +0100

    Added main file. Added Reservations and Schedule class

---
+++ ProjectB/Reservation.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectB
+{
+    internal class Reservation
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Date_time { get; set; }
+        public int Person_count { get; set; }
+
+        public Reservation(
+            string name,
+            string email,
+            string date_time,
+            int person_count)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Date_time = date_time;
+            this.Person_count = person_count;
+        }
+    }
+}
---
+++ ProjectB/Schedule.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ProjectB
+{
+    public static class Schedule
+    {
+        static readonly List<Reservation> Reservations = new();
+
+        public static void ListReservations()
+        {
+            foreach (var item in Reservations)
+                Console.WriteLine($"{item.Name} at {item.Date_time} with {item.Person_count} persons.");
+        }
+
+        static int FindReservationIndex(string name_of_res)
+        {
+            int index = -1;
+            for (int i = 0; i < Reservations.Count; i++)
+            {
+                if (Reservations[i].Name.ToLower() == name_of_res.ToLower())
+                    index = i;
+            }
+            return index;
+        }
+        public static void AddReservation(
+            string name,
+            string email,
+            string date_time,
+            int person_count)
+        {
+            var new_res = new Reservation(name, email, date_time, person_count);
+            Reservations.Add(new_res);
+            Console.WriteLine("Added Reservation");
+        }
+
+        public static void RemoveReservation(string name_of_res)
+        {
+            int remove_index = FindReservationIndex(name_of_res);
+
+            if (remove_index != -1)
+            {
+                Reservations.RemoveAt(remove_index);
+                Console.WriteLine("Removed reservation.");
+            }
+            else
+            {
+                Console.WriteLine("Could not find reservation");
+            }
+
+        }
+
+        public static void EditName(string name_of_res, string value_to_change_to)
+        {
+            int edit_index = FindReservationIndex(name_of_res);
+
+            if (edit_index != -1)
+            {
+                Reservations[edit_index].Name = value_to_change_to;
+                Console.WriteLine("Edited name of reservation.");
+            }
+            else
+            {
+                Console.WriteLine("Could not find reservation");
+            }
+        }
+        public static void EditEmail(string name_of_res, string value_to_change_to)
+        {
+            int edit_index = FindReservationIndex(name_of_res);
+
+            if (edit_index != -1)
+            {
+                Reservations[edit_index].Email = value_to_change_to;
+                Console.WriteLine("Edited email of reservation.");
+            }
+            else
+            {
+                Console.WriteLine("Could not find reservation");
+            }
+        }
+        public static void EditDateTime(string name_of_res, string value_to_change_to)
+        {
+            int edit_index = FindReservationIndex(name_of_res);
+
+            if (edit_index != -1)
+            {
+                Reservations[edit_index].Date_time = value_to_change_to;
+                Console.WriteLine("Edited DateTime of reservation.");
+            }
+            else
+            {
+                Console.WriteLine("Could not find reservation");
+            }
+        }
+        public static void EditPersonCount(string name_of_res, int value_to_change_to)
+        {
+            int edit_index = FindReservationIndex(name_of_res);
+
+            if (edit_index != -1)
+            {
+                Reservations[edit_index].Person_count = value_to_change_to;
+                Console.WriteLine("Edited person count of reservation.");
+            }
+            else
+            {
+                Console.WriteLine("Could not find reservation");
+            }
+        }
+
+    }
+}
