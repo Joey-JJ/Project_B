b5be1ad6e270bd105d8dceb32c460bb33c664136
Joey van der Valk <joeyvandervalk@gmail.com>
2022-03-20 20:00:23 +0100

    Added code van mees

--- ProjectB/Program.cs
+++ ProjectB/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ProjectB
+﻿using System;
+
+
+namespace ProjectB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-        }
+            System.Console.WriteLine("Hello");
+            bool startingscreeninValidAnswer = true;
+            while (startingscreeninValidAnswer)
+            {
+                try
+                {
+                    string startingscreen = "Hello, welcome to paps patat \n Press 1 for login and account creation \n Press 2 to see reservations";
+                    Console.WriteLine(startingscreen);
+                    int startingscreenoutput = Convert.ToInt32(Console.ReadLine());
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Something went wrong, try again!");
+                }
+            }
+         }
     }
 }
