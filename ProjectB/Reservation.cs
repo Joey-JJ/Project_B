730bf3ef93542346c969c868bfee8123f30785ff
Joey-JJ <58442698+Joey-JJ@users.noreply.github.com>
2022-03-26 15:21:15 +0100

    Update Reservation.cs

--- ProjectB/Reservation.cs
+++ ProjectB/Reservation.cs
@@ -1,22 +1,141 @@
-﻿namespace ProjectB
-{
-    internal class Reservation
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Date_time { get; set; }
-        public int Person_count { get; set; }
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
 
-        public Reservation(
+class Reservation
+{
+    public static List<Reservation> Reservations = new();
+    private static string FileName = "Reservations.json";
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string DateTime { get; set; }
+    public int PersonCount { get; set; }
+
+    public Reservation(
             string name,
             string email,
-            string date_time,
-            int person_count)
+            string datetime,
+            int personcount)
+    {
+        this.Name = name;
+        this.Email = email;
+        this.DateTime = datetime;
+        this.PersonCount = personcount;
+    }
+
+    public static void SaveReservations()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(Reservation.Reservations, options);
+        File.WriteAllText(FileName, jsonString);
+        System.Console.WriteLine("Saved Reservations");
+    }
+
+    public static void LoadReservations()
+    {
+        string jsonString = File.ReadAllText(FileName);
+        Reservations = JsonSerializer.Deserialize<List<Reservation>>(jsonString);
+        System.Console.WriteLine("Loaded Reservations");
+    }
+
+    public static void ListReservations()
+    {
+        foreach (var item in Reservations)
+            Console.WriteLine($"{item.Name} at {item.DateTime} with {item.PersonCount} persons.");
+    }
+
+    static int FindReservationIndex(string nameOfRes)
+    {
+        int index = -1;
+        for (int i = 0; i < Reservations.Count; i++)
         {
-            this.Name = name;
-            this.Email = email;
-            this.Date_time = date_time;
-            this.Person_count = person_count;
+            if (Reservations[i].Name.ToLower() == nameOfRes.ToLower())
+                index = i;
+        }
+        return index;
+    }
+    public static void AddReservation(
+        string name,
+        string email,
+        string date_time,
+        int person_count)
+    {
+        var newRes = new Reservation(name, email, date_time, person_count);
+        Reservations.Add(newRes);
+        Console.WriteLine("Added Reservation");
+    }
+
+    public static void RemoveReservation(string nameOfRes)
+    {
+        int removeIndex = FindReservationIndex(nameOfRes);
+
+        if (removeIndex != -1)
+        {
+            Reservations.RemoveAt(removeIndex);
+            Console.WriteLine("Removed reservation.");
+        }
+        else
+        {
+            Console.WriteLine("Could not find reservation");
+        }
+
+    }
+
+    public static void EditName(string nameOfRes, string valueToChangeTo)
+    {
+        int editIndex = FindReservationIndex(nameOfRes);
+
+        if (editIndex != -1)
+        {
+            Reservations[editIndex].Name = valueToChangeTo;
+            Console.WriteLine("Edited name of reservation.");
+        }
+        else
+        {
+            Console.WriteLine("Could not find reservation");
+        }
+    }
+    public static void EditEmail(string nameOfRes, string valueToChangeTo)
+    {
+        int editIndex = FindReservationIndex(nameOfRes);
+
+        if (editIndex != -1)
+        {
+            Reservations[editIndex].Email = valueToChangeTo;
+            Console.WriteLine("Edited email of reservation.");
+        }
+        else
+        {
+            Console.WriteLine("Could not find reservation");
+        }
+    }
+    public static void EditDateTime(string nameOfRes, string valueToChangeTo)
+    {
+        int editIndex = FindReservationIndex(nameOfRes);
+
+        if (editIndex != -1)
+        {
+            Reservations[editIndex].DateTime = valueToChangeTo;
+            Console.WriteLine("Edited DateTime of reservation.");
+        }
+        else
+        {
+            Console.WriteLine("Could not find reservation");
+        }
+    }
+    public static void EditPersonCount(string nameOfRes, int valueToChangeTo)
+    {
+        int editIndex = FindReservationIndex(nameOfRes);
+
+        if (editIndex != -1)
+        {
+            Reservations[editIndex].PersonCount = valueToChangeTo;
+            Console.WriteLine("Edited person count of reservation.");
+        }
+        else
+        {
+            Console.WriteLine("Could not find reservation");
         }
     }
 }
