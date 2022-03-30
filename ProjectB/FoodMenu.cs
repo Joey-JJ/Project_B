15018228cd6c1cc15a403918bf62b9266d1b030f
ValencioTheOG <99248292+ValencioTheOG@users.noreply.github.com>
2022-03-30 16:35:27 +0200

    Update FoodMenu.cs

--- ProjectB/FoodMenu.cs
+++ ProjectB/FoodMenu.cs
@@ -42,10 +42,6 @@ namespace ProjectB
                 { "Chateau Mouton Rothschild Pauillac", 8.00},
                 { "Chateau Margaux", 8.00},
                 { "Bordeaux red", 8.00}
-
-
-
-
         };
         public static void PrintMenu()
         {
@@ -54,9 +50,32 @@ namespace ProjectB
                Console.WriteLine("{0}, price = {1}", Item.Key, Item.Value);
                 
             }
-
-
         }
+         public static void PrintAppetizers()
+        {
+            Console.WriteLine("The Appetizers:\nOeuf en cocotte  * $8.00 *\nFrench Onionsoup * $8.00 *\nPiperade * $8.00 *");
+        }
+        public static void PrintMainCourse()
+        {
+            Console.WriteLine("The Maincourses:\nVegetable gratin * $14.00 *\nEntrecote bordelaise * $20.00 *\nCoq au vin * $24.00 *\nOisters * $30.00 *\nConfit de canard * $25.00 *\nHomard aux aromates * $24.00 *");
+        }
+        public static void PrintDesserts()
+        {
+            Console.WriteLine("The Desserts:\nLemon cake * $8.00 *\nMoelleux au chocolat* $10.00 *\nCrepes: (choose from:banana, strawberry, blueberry and with(out) whipped cream white/milk/dark chocolatesauce) * $8.00 *\nChocolate souffle * $14.00 *\nCreme brulee * $14.00 *");
+        }
+        public static void PrintVeganMainCourses()
+        {
+            Console.WriteLine("The Vegan Maincourses:\nVegan choucroute garnie * $14.00 *\n Champignon bourguignon * $20.00 *\nRatatouille * $18.00 *\nFrench lentil & greens soup * $18.00 *\n Salade nicoise * $25.00 *\n Vegan Escargot * $20.00 *");
+        }
+        public static void PrintVeganDesserts()
+        {
+            Console.WriteLine("The Vegan Desserts:\nicecream of choice (blueberry, strawberry, lemon, chocolate and vanilla) * $4.00 *\nBrownie * $8.00 *\nVanilla cake * $10.00 *\nVegan Crepes * $8.00 *\nCreme Brulee * $14.00 *");
+        }
+        public static void PrintDrinks()
+        {
+            Console.WriteLine("Drinks:\nCoca cola * $ 2.50 *\nIce Tea (sparkling, normal, peach, green tea)* $2.50 *\nFristi * $2.00 *\nChocomel * $2.00 *\nOasis * $2.00 *\nWater * $ *2.00\nChateau mouton rothschild pauillac * $12.00 *\nChateau margaux * $12.00 *\nBordeax red * $12.00 *");
+        }
+        
     }
 
 }
