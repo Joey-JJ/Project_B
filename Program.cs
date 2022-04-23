869f93e918a13270c7e7c78aa896146ae0e68a8f
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-04-23 22:51:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WerkRoosterProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string userInputWorkSchedule;
+            Console.WriteLine("Welcome to the WorkSchedule Menu\n [1] to add Shifts\n [2] for deleting shifts\n [3] to edit shift \n [4] to list all shifts ");
+            while (true)
+            {
+                userInputWorkSchedule = Console.ReadLine();
+                if (userInputWorkSchedule == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Add shift beneath");
+                    Console.ReadLine();
+                }
+
+                else if (userInputWorkSchedule == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Delete Shifts");
+                }
+
+                else if (userInputWorkSchedule == "3")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Edit Shifts");
+                }
+
+                else if (userInputWorkSchedule == "4")
+                {
+                    Console.Clear();
+                    Console.WriteLine("List of all Shifts");
+
+
+                    Shift shift = new Shift()
+                    {
+                        Id = 10478,
+                        Name = "Lorenzo",
+                        dag = "Zaterdag 23 april ",
+                        werktijd = "16:00-23:00",
+                        functies = new List<string>()
+                    {
+                    "Koken",
+                    "Schoonmaken",
+                    "Tavelsbedekken"
+                    }
+                };
+
+                string strResultJson = JsonConvert.SerializeObject(shift);
+                //Console.WriteLine(strResultJson);
+                File.WriteAllText(@"Duty.json", strResultJson);
+                //Console.WriteLine("Stored!");
+
+
+                strResultJson = File.ReadAllText(@"Duty.json");
+                Shift resultShift = JsonConvert.DeserializeObject<Shift>(strResultJson);
+                Console.WriteLine(resultShift.ToString());
+
+                }
+                else
+                {
+                    System.Console.WriteLine("Please enter a valid number");
+                }
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Shift.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WerkRoosterProgram
+{
+    class Shift
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string dag { get; set; }
+        public string werktijd { get; set; }
+        public List<string> functies { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Shift Information: \n\tId:{0}, \n\tName:{1}, \n\tdag:{2}, \n\twerktijd: {3}, " +
+                                         "\n\tfuncties: {4}", Id, Name, dag, werktijd, string.Join(",", functies.ToArray()));
+        }
+
+    }
+}
+
